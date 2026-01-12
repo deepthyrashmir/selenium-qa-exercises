9e22a1c7046f9412ad33e721f4866fd72f9942df
deepthyrashmir <deepthyrashmir@gmail.com>
2026-01-12 23:37:00 +0530

    Update DynamicLoadingTest.java

--- src/test/java/tests/DynamicLoadingTest.java
+++ src/test/java/tests/DynamicLoadingTest.java
@@ -3,8 +3,8 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -18,47 +18,18 @@ public class DynamicLoadingTest {
     WebDriver driver;
     WebDriverWait wait;
 
-    @BeforeMethod
+    @BeforeClass
     public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
     @Test
-    public void verifyHelloWorldIsDisplayed() throws Exception {
+    public void verifyHelloWorldText() throws Exception {
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
 
-        driver.findElement(By.xpath("//button[text()='Start']")).click();
+        driver.findElement(By.cssSelector("#start button")).click();
 
-        WebElement resultText = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.id("finish"))
-        );
-
-        Assert.assertEquals(resultText.getText(), "Hello World!",
-                "Expected text did not match");
-
-        takeScreenshot("hello_world.png");
-    }
-
-    private void takeScreenshot(String fileName) throws Exception {
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-        File folder = new File("screenshots");
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-
-        File dest = new File(folder, fileName);
-        Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-}
+        WebElement helloText = wa
