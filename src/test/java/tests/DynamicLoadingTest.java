9f204591151f4d84b390f2eaf1213bec5db27fdd
deepthyrashmir <deepthyrashmir@gmail.com>
2026-01-12 23:39:12 +0530

    Update DynamicLoadingTest.java

--- src/test/java/tests/DynamicLoadingTest.java
+++ src/test/java/tests/DynamicLoadingTest.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -32,4 +34,35 @@ public class DynamicLoadingTest {
 
         driver.findElement(By.cssSelector("#start button")).click();
 
-        WebElement helloText = wa
+        WebElement helloText = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("finish"))
+        );
+
+        Assert.assertEquals(
+                helloText.getText(),
+                "Hello World!",
+                "Hello World text not displayed"
+        );
+
+        takeScreenshot("hello_world.png");
+    }
+
+    private void takeScreenshot(String fileName) throws Exception {
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        File folder = new File("screenshots");
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        File dest = new File(folder, fileName);
+        Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
