2b0d318a420ff82b1a27d09d8456bdb7f4c327bf
deepthyrashmir <deepthyrashmir@gmail.com>
2026-01-13 00:31:52 +0530

    Update WebTablesTest.java

--- src/test/java/tests/WebTablesTest.java
+++ src/test/java/tests/WebTablesTest.java
@@ -23,7 +23,7 @@ public class WebTablesTest {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        // Click Add button
+        
         WebElement addBtn = wait.until(
                 ExpectedConditions.elementToBeClickable(By.id("addNewRecordButton"))
         );
