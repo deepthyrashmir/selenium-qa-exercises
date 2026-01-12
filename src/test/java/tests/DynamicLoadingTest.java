5559d73e8b0cdb26e2b76fc7fd549e3ac55673b4
deepthyrashmir <deepthyrashmir@gmail.com>
2026-01-12 23:43:35 +0530

    Update DynamicLoadingTest.java

--- src/test/java/tests/DynamicLoadingTest.java
+++ src/test/java/tests/DynamicLoadingTest.java
@@ -3,66 +3,50 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 
 public class DynamicLoadingTest {
 
     WebDriver driver;
-    WebDriverWait wait;
 
-    @BeforeClass
+    @BeforeMethod
     public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
     }
 
     @Test
     public void verifyHelloWorldText() throws Exception {
-        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
 
-        driver.findElement(By.cssSelector("#start button")).click();
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
 
-        WebElement helloText = wait.until(
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement text = wait.until(
                 ExpectedConditions.visibilityOfElementLocated(By.id("finish"))
         );
 
-        Assert.assertEquals(
-                helloText.getText(),
-                "Hello World!",
-                "Hello World text not displayed"
-        );
+        Assert.assertEquals(text.getText(), "Hello World!");
 
-        takeScreenshot("hello_world.png");
+        // Screenshot
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File src = ts.getScreenshotAs(OutputType.FILE);
+        Files.copy(src.toPath(), new File("screenshots/hello_world.png").toPath());
     }
 
-    private void takeScreenshot(String fileName) throws Exception {
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-        File folder = new File("screenshots");
-        if (!folder.exists()) {
-            folder.mkdir();
-        }
-
-        File dest = new File(folder, fileName);
-        Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-    }
-
-    @AfterClass
+    @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
     }
 }
