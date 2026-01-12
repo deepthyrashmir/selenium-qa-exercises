c4b87aa327762604ded0f077c8e9933c72d6dcf3
deepthyrashmir <deepthyrashmir@gmail.com>
2026-01-12 22:06:52 +0530

    Add Dynamic Loading Selenium test

---
+++ src/test/java/tests/DynamicLoadingTest.java
@@ -0,0 +1,50 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.*;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.time.Duration;
+
+public class DynamicLoadingTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+    }
+
+    @Test
+    public void verifyHelloWorldText() throws Exception {
+
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement text = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("finish"))
+        );
+
+        Assert.assertEquals(text.getText(), "Hello World!");
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File src = ts.getScreenshotAs(OutputType.FILE);
+        Files.copy(src.toPath(), new File("screenshots/hello_world.png").toPath());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
